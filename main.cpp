c854987d56e3923c09f402d34463a2f24277c253
Maya Archambault <mayarchambault@gmail.com>
2020-02-18 14:31:35 -0500

    Reimplemented Main
    Finally reimplemented main function using all of the new class

--- main.cpp
+++ main.cpp
@@ -183,3 +183,22 @@ protected:
 	int nb_movement;
 
 }; //class Hanoi_txt
+
+
+int main(int argc, char const *argv[])
+{
+	int max_height;
+	cout << "Number of disc for the game : ";
+	cin >> max_height;
+	Hanoi the_game;
+	the_game.Initialise(max_height);
+	Hanoi_txt interface(the_game);
+	while( not the_game.GameOver() )
+	{
+		interface.DisplayGame();
+		interface.Step();
+	}
+	interface.DisplayGame();
+	cout << "Congrats you finished the game in " << interface.Nb_Mouvements() << " movements!" << endl;
+	return 0;
+}
